0bd775aec250c0fceaadb172cf6582d3e8448a8e
Deimek <333olegovich333@gmail.com>
2025-01-19 15:32:49 +0100

    add

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+const ulTodo = document.querySelector('.todo__list');
+const btnAddTodo = document.querySelector('.todo__btn');
+btnAddTodo.addEventListener('click', () => {
+
+    const inputTodo = document.querySelector('.todo__input')
+    const inputTodoValue = inputTodo.value.trim();
+
+    if (inputTodoValue === '') {
+        return;
+    }
+    const liAddTodo = document.createElement('li');
+    liAddTodo.textContent = inputTodoValue;
+    ulTodo.appendChild(liAddTodo);
+
+
+    const btnDel = document.createElement('button');
+    btnDel.textContent = 'X';
+    liAddTodo.appendChild(btnDel);
+    // btnDel.addEventListener('click', () => {
+    //     liAddTodo.remove();
+    // });
+
+    inputTodo.value = '';
+
+    ulTodo.addEventListener('click', (event) => {
+        if (event.target.tagName === 'BUTTON') {
+            event.target.parentElement.remove();
+        }
+    })
+
+})
+
+
+
